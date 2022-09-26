69eaed890c307e43f62ec467e8ad8e836636057d
“Skotin <sascha.scotin2011@list.ru>
2022-09-26 16:18:54 +0300

    DONE! TASK 1

---
+++ 4Tasks/4Tasks/main.cpp
@@ -0,0 +1,25 @@
+#include "struct_stl.h"
+
+int main()
+{
+	task1 v;
+	v.generate();
+	v.max_min();
+	v.add4_element();
+	v.print();
+	v.less_10();
+	v.print();
+	v.find_over20();
+	v.even_multiply_3();
+	v.print();
+	v.random();
+	v.print();
+	v.del_over50();
+	v.print();
+	v.count_print();
+	v.print();
+	v.clear();
+	v.print();
+	v.clear();
+	v.print();
+}
