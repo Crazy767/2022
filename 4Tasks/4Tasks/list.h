131649eaeb2de6edcc68f4943400cd90b43800c3
“Skotin <sascha.scotin2011@list.ru>
2022-09-27 12:22:33 +0300

    DONE! TASK2

---
+++ 4Tasks/4Tasks/list.h
@@ -0,0 +1,55 @@
+#include <list>
+#include <iostream>
+
+struct task2
+{
+private:
+	std::list<int> l;
+public:
+	void add5_back()
+	{
+		srand(time(0));
+		for (int i = 0; i != 5; i++)
+			l.push_back(rand() % 1000);
+	}
+
+	void print_begin() { std::cout << "First element: " << l.front(); }
+
+	void add2_front()
+	{
+		srand(time(0));
+		l.push_front(rand() % 1000);
+		l.push_front(rand() % 1000);
+	}
+
+	void del4_element() { l.erase(std::next(l.begin(), 3)); }
+
+	void add_random3()
+	{
+		srand(time(0));
+		int number = rand() % 1000;
+		for (int i = 0; i != 3; i++) { l.insert(std::next(l.begin(), rand() % l.size()), number); }
+	}
+
+	void pop_back() { l.pop_back(); }
+	void pop_front() { l.pop_front(); }
+
+	void add2_middle()
+	{
+		srand(time(0));
+		l.splice(std::next(l.begin(), int(l.size() / 2)), std::list<int>(2, rand() % 100));
+	}
+
+	void unique() { l.unique(); }
+
+	void clear() { l.clear(); }
+
+	void print_is_clear() 
+	{ std::cout << std::endl << (l.size() == NULL) ? "List Clear" : "List Not Clear"; }
+
+	void print()
+	{
+		std::cout << std::endl << "List: ";
+		for (auto i : l) { std::cout << i << " "; }
+	}
+};
--- 4Tasks/4Tasks/main.cpp
+++ 4Tasks/4Tasks/main.cpp
@@ -1,25 +1,42 @@
-#include "vector_stl.h"
-
+#include "vector.h"
+#include "list.h"
 int main()
 {
-	task1 v;
-	v.generate();
-	v.max_min();
-	v.add4_element();
-	v.print();
-	v.less_10();
-	v.print();
-	v.find_over20();
-	v.even_multiply_3();
-	v.print();
-	v.random();
-	v.print();
-	v.del_over50();
-	v.print();
-	v.count_print();
-	v.print();
-	v.clear();
-	v.print();
-	v.clear();
-	v.print();
+	//task1 v;
+	//v.generate();
+	//v.max_min();
+	//v.add4_element();
+	//v.print();
+	//v.less_10();
+	//v.print();
+	//v.find_over20();
+	//v.even_multiply_3();
+	//v.print();
+	//v.random();
+	//v.print();
+	//v.del_over50();
+	//v.print();
+	//v.count_print();
+	//v.print();
+	//v.clear();
+	//v.print();
+	//v.clear();
+	//v.print();
+
+	task2 l;
+	l.add5_back();
+	l.print_begin();
+	l.add2_front();
+	l.del4_element();
+	l.print();
+	l.add_random3();
+	l.print();
+	l.add2_middle();
+	l.print();
+	l.unique();
+	l.print();
+	l.print_is_clear();
+	l.clear();
+	l.print();
+	l.print_is_clear();
 }
